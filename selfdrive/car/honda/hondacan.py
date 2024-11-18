802de8f9d8d76a8dc2e5bfee235289db26d84ca8
zhou jielei <zhou-jielei@gmo.jp>
2024-11-18 12:55:03 +0900

    revert and fix

--- selfdrive/car/honda/hondacan.py
+++ selfdrive/car/honda/hondacan.py
@@ -97,7 +97,7 @@ def create_acc_commands(packer, CAN, enabled, active, accel, gas, stopping_count
   if car_fingerprint in HONDA_BOSCH_RADARLESS:
     acc_control_values.update({
       "CONTROL_ON": enabled,
-      "IDLESTOP_ALLOW": 1,  # allow idle stop after 4 seconds (50 Hz)
+      "IDLESTOP_ALLOW": stopping_counter > 200,  # allow idle stop after 4 seconds (50 Hz)
     })
   else:
     acc_control_values.update({
